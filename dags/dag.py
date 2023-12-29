4a3ffffe09fb747a6384e5832945f5298f89ddcd
okuu <filipp.petuhhov@gmail.com>
2023-12-30 00:49:56 +0200

    json read flow is now batch-centered. added progress file logic

--- dags/dag.py
+++ dags/dag.py
@@ -48,6 +48,21 @@ NORM_JSON_INPUT = '/tmp/data/norm_jsons/input'
 NORM_JSON_SUCCESS = '/tmp/data/norm_jsons/success'
 NORM_JSON_FAIL = '/tmp/data/norm_jsons/fail'
 
+PROGRESS_FILE_PATH = '/tmp/data/progress.json'
+
+def load_progress():
+    try:
+        if os.path.exists(PROGRESS_FILE_PATH):
+            with open(PROGRESS_FILE_PATH, 'r') as file:
+                return json.load(file)
+    except json.JSONDecodeError:
+        return {}
+    return {}
+
+def save_progress(progress):
+    with open(PROGRESS_FILE_PATH, 'w') as file:
+        json.dump(progress, file)
+
 upload_to_staging_db = DAG(
     dag_id='process_raw_and_upload_staging', # name of dag
     schedule_interval='*/30 * * * *', 
@@ -139,9 +154,13 @@ def set_dict(d, key, item):
     else:
         d[key] = set([item]) 
 
-def process_file(path, output_path, failed_lines, norm_datas, n_lines, batch_size):
+def process_file(path, output_path, failed_lines, norm_datas, progress, batch_size, n_batches):
+    n_lines = progress.get(path, 0)
+    print(n_lines)
     with open(path, 'rb') as f:
-        for line in f:
+        for line_number, line in enumerate(f):
+            if line_number < n_lines:
+                continue
             try:
                 data = json.loads(line.strip())
             except:
@@ -155,26 +174,33 @@ def process_file(path, output_path, failed_lines, norm_datas, n_lines, batch_siz
                 continue
             
             n_lines += 1
-            if n_lines > batch_size:
+            if n_lines % batch_size == 0:
                 # Not handling exceptions here as it's perferrable to crash as errors here are not related to input data
                 with open(f'{output_path}/{uuid.uuid1()}.json', 'w') as f:
                     json.dump(norm_datas, f, indent=4)
-                n_lines = 0
                 norm_datas = []
-            
-    return n_lines, norm_datas
+                n_batches -= 1
+                print(n_batches)
 
-def load_and_normalize(data_path, output_path, success_path, fail_path, batch_size=100):
+            if n_batches == 0:
+                progress[path] = n_lines
+                save_progress(progress)
+                break
+
+    return norm_datas
+
+def load_and_normalize(data_path, output_path, success_path, fail_path, batch_size, n_batches):
     norm_datas = []
-    n_lines = 0
+    progress = load_progress()
+    print(progress)
     for path in glob(data_path):
         failed_lines = []
         try:
-            n_lines, norm_datas = process_file(path, output_path, failed_lines, norm_datas, n_lines, batch_size)
+            norm_datas = process_file(path, output_path, failed_lines, norm_datas, progress, batch_size, n_batches)
             if len(norm_datas) > 0:
                 with open(f'{output_path}/{uuid.uuid1()}.json', 'w') as f:
                     json.dump(norm_datas, f, indent=4)
-            shutil.move(path, Path(success_path)/Path(path).name)
+                shutil.move(path, Path(success_path)/Path(path).name)
         except:
             shutil.move(path, Path(fail_path)/Path(path).name)
         current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -194,7 +220,8 @@ normalize_json_task = PythonOperator(
         'output_path': NORM_JSON_INPUT,
         'success_path': RAW_DATA_SUCCESS,
         'fail_path': RAW_DATA_FAIL,
-        'batch_size': 100
+        'batch_size': 100,
+        'n_batches': 5
     },
 )
 
